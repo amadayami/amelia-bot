0db2272d954acb7919525b7e15113a864d643865
Fen Michael <amadayamichael@gmail.com>
2024-10-01 15:55:45 -0400

    Adds getreports and getwarnings commands.

---
+++ commands/mod/getwarnings.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const warninglog = './resources/warninglog.json';
+const { mod } = require('../../resources/roleids.json');
+
+function warningToString(warnings){
+	let warningStr = "";
+	let count = 0;
+	for(const warning in warnings){
+		count++;
+		warningStr += `warning #${warning}:\ndate: ${new Date(warnings[warning].timestamp * 1000)}\nreason: ${warnings[warning].reason}`;
+		warningStr += "\n\n";
+	}
+	warningStr += `User has been warned ${count} times`;
+	return warningStr;
+}
+
+function retrieveData(user){
+	let rawdata = fs.readFileSync(warninglog);
+	let warnings = JSON.parse(rawdata);
+	if(warnings.hasOwnProperty(user)){
+		return warningToString(warnings[user]);
+	}
+	else return 0;
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName('getwarnings')
+	.setDescription('Retrieves all warnings sent to a user')
+	.addUserOption(option =>
+		option
+			.setName('user')
+			.setDescription('user to search')
+			.setRequired(true)
+		),
+			
+			
+	async execute(interaction) {
+		const user = interaction.options.getUser('user');
+		let resp = retrieveData(user);
+		if(resp === 0){
+			await interaction.reply({ content: "No warnings found for user.", ephemeral: true });
+		}
+		else await interaction.reply({ content: resp, ephemeral: true });
+	}		
+}	
